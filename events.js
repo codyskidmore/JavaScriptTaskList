256ef7b6f44a491f3edb97c40d213da1a5882a38
Cody Skidmore <cody.skidmore@vueworks.com>
2019-07-25 14:30:33 -0400

    Organized the code to move it into smaller chunks. Got a little ahead of myself by adding the delete task code, local storage, & clear tasks features. Oh well.

--- events.js
+++ events.js
@@ -1,15 +1,3 @@
-const title = "Task List [Part 1] - UI & Add Task Items";
-document.title = title;
-
-const form = document.querySelector('#task-form');
-const taskList = document.querySelector('#task-collection');
-const clearBtn = document.querySelector('#clear-button');
-const taskFilter = document.querySelector('#task-filter');
-const taskInput = document.querySelector('#task-input');
-const errors = document.querySelector('#errors');
-const taskRepository = new TaskRepository();
-const domUpdater = new DomUpdater();
-
 function addItemEvent(e){
     try{
         if (taskInput.value === '')
@@ -25,7 +13,6 @@ function addItemEvent(e){
         errors.innerHTML = err.message;
     }
 }
-
 function clearTasksEvent(e){
     try{
         taskRepository.clearTasks();
@@ -53,16 +40,3 @@ function LoadExistingTasks(){
         errors.innerHTML = err.message;
     }
 }
-
-// Load event listeners
-(function(){
-    try{
-        form.addEventListener(FormEvents.Submit(), addItemEvent);
-        clearBtn.addEventListener(MouseEvents.Click(), clearTasksEvent);
-        LoadExistingTasks();
-        console.log('it ran.');    
-    }
-    catch(err){
-        errors.innerHTML = err.message;
-    }
-})();
