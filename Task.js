f5469360f39f8d658af95ef718aed5dfd757644c
Cody Skidmore <cody.skidmore@vueworks.com>
2019-07-25 12:47:04 -0400

    Check point before continuing.

---
+++ Task.js
@@ -0,0 +1,6 @@
+class Task{
+    constructor(name, id){
+        this.name = name;
+        this.id = id;
+    }
+}
--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-const title = "";
+const title = "Task List [Part 1] - UI & Add Task Items";
 document.title = title;
+
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('#task-collection');
+const clearBtn = document.querySelector('#clear-button');
+const taskFilter = document.querySelector('#task-filter');
+const taskInput = document.querySelector('#task-input');
+
+//console.log(form, taskList, clearBtn, taskFilter, taskInput);
+
+const taskRepository = new TaskRepository();
+
+////////// Just a bunch of tests verifying the repository works.
+//const newTask = taskRepository.createTask("Walk the dog 4");
+// const task = taskRepository.getTaskById(4);
+// taskRepository.deleteTask(task.id);
+//console.log(newTask);
+// taskRepository.updateTask("New Task Name 3", 2);
+//const task = taskRepository.getTaskByName('New Task Name');
+
+//console.log(task);
